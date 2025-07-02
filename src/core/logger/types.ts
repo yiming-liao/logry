20827dc856c94d2339475ade690e8c34f1e59cc0
yiming-liao <yimingliao.official@gmail.com>
2025-07-02 09:13:57 +0700

    feat(types): change log method meta argument type for greater flexibility

--- src/core/logger/types.ts
+++ src/core/logger/types.ts
@@ -1,7 +1,12 @@
 import type { FormatterConfig } from "@/modules/formatters/types";
 import type { NormalizerConfig } from "@/modules/normalizers/types";
 import type { Level } from "@/shared/types";
-import type { RawContext, RawMeta, RawScope } from "@/shared/types/log-fields";
+import type {
+  RawContext,
+  RawMessage,
+  RawMeta,
+  RawScope,
+} from "@/shared/types/log-fields";
 
 /** Options for `child` method in Logger */
 export type ChildOptions = {
@@ -23,14 +28,14 @@ export type LogOptions = {
 
 /** Arguments accepted by log methods */
 type LogArgs =
-  | [string] // message
-  | [Record<string, unknown>] // meta
-  | [string, Record<string, unknown>] // message, meta
-  | [Record<string, unknown>, string] // meta, message
+  | [RawMessage] // message
+  | [RawMeta] // meta
+  | [string, RawMeta] // message, meta
+  | [RawMeta, string] // meta, message
   | [string, LogRuntimeOptions] // message, runtime options
-  | [Record<string, unknown>, LogRuntimeOptions] // meta, runtime options
-  | [Record<string, unknown>, string, LogRuntimeOptions] // meta, message, runtime options
-  | [string, Record<string, unknown>, LogRuntimeOptions]; // message, meta, runtime options
+  | [RawMeta, LogRuntimeOptions] // meta, runtime options
+  | [RawMeta, string, LogRuntimeOptions] // meta, message, runtime options
+  | [string, RawMeta, LogRuntimeOptions]; // message, meta, runtime options
 
 /** Log method bound to a level */
 export type BoundLogMethod = (...args: LogArgs) => void;
